fe2e29dd3b06db791ccb5a172f3e6fc38fc4c9bc
Kasey Klipsch <kklipsch@mediamath.com>
2015-03-17 21:30:59 -0500

    add list package

---
+++ list/list.go
@@ -0,0 +1,70 @@
+package list
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func execList(args ...string) *exec.Cmd {
+	arguments := append([]string{"list"}, args...)
+	return exec.Command("go", arguments...)
+}
+
+func listOutput(cmd *exec.Cmd) ([]string, error) {
+	out, err := cmd.Output()
+	return filterEmpty(strings.Split(strings.TrimSpace(fmt.Sprintf("%s", out)), "\n")), err
+}
+
+func filterEmpty(sl []string) []string {
+	filtered := make([]string, 0)
+	for _, s := range sl {
+		if strings.TrimSpace(s) != "" {
+			filtered = append(filtered, s)
+		}
+	}
+
+	return filtered
+}
+
+func Query(paths ...string) (map[string]string, []error) {
+	cmd, err := listQueryTemplate(paths...)
+	if err != nil {
+		return map[string]string{}, []error{err}
+	}
+
+	errors := make([]error, 0)
+	results := make(map[string]string)
+	for _, res := range cmd {
+		path, dir, parseErr := queryParse(res)
+		if parseErr != nil {
+			errors = append(errors, parseErr)
+		} else {
+			results[path] = dir
+		}
+	}
+
+	return results, errors
+}
+
+func listQueryTemplate(paths ...string) ([]string, error) {
+	args := append([]string{"-e", "-f", "'{{ .ImportPath }}|{{ .Dir }}|{{ .Error }}'"}, paths...)
+	return listOutput(execList(args...))
+}
+
+func queryParse(result string) (string, string, error) {
+	result = strings.TrimPrefix(result, "'")
+	result = strings.TrimSuffix(result, "'")
+	split := strings.Split(result, "|")
+	if len(split) != 3 {
+		return "", "", fmt.Errorf("Unable to parse result: %v", result)
+	}
+
+	errString := split[2]
+	var err error
+	if errString != "<nil>" {
+		err = fmt.Errorf(errString)
+	}
+
+	return split[0], split[1], err
+}
---
+++ list/list_test.go
@@ -0,0 +1,116 @@
+package list
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSinglePathResult(t *testing.T) {
+	cmd, err := listOutput(execList("os/exec"))
+	if !eq(cmd, sl("os/exec")) {
+		t.Errorf("|%v|%v|", cmd, err)
+	}
+}
+
+func TestMultiPathResult(t *testing.T) {
+	cmd, err := listOutput(execList("os/..."))
+	if !eq(cmd, sl("os", "os/exec", "os/signal", "os/user")) {
+		t.Errorf("|%v|%v|", cmd, err)
+	}
+}
+
+func TestFailPathResult(t *testing.T) {
+	cmd, err := listOutput(execList("os/foodog"))
+	if len(cmd) > 0 || err == nil {
+		t.Errorf("|%v|%v|", cmd, err)
+	}
+}
+
+func TestFailPathResultWithPassingToo(t *testing.T) {
+	cmd, err := listOutput(execList("os/foodog", "os/signal"))
+	if !eq(cmd, sl("os/signal")) || err == nil {
+		t.Errorf("|%v|%v|", cmd, err)
+	}
+}
+
+func TestFailPathResultWithErrorsOff(t *testing.T) {
+	cmd, err := listOutput(execList("-e", "os/foodog"))
+	if !eq(cmd, sl("os/foodog")) || err != nil {
+		t.Errorf("|%v|%v|", cmd, err)
+	}
+}
+
+func TestFailPathResultWithPassingTooWithErrorsOff(t *testing.T) {
+	cmd, err := listOutput(execList("-e", "os/foodog", "os/signal"))
+	if !eq(cmd, sl("os/foodog", "os/signal")) || err != nil {
+		t.Errorf("|%v|%v|", cmd, err)
+	}
+}
+
+func eq(slice1 []string, slice2 []string) bool {
+	return reflect.DeepEqual(slice1, slice2)
+}
+
+func sl(items ...string) []string {
+	return items
+}
+
+func TestQueryParse(t *testing.T) {
+	path, dir, err := queryParse("'text/template|/usr/local/Cellar/go/1.4.2/libexec/src/text/template|<nil>'")
+
+	if path != "text/template" {
+		t.Errorf("%v|%v|%v", path, dir, err)
+	}
+
+	if dir != "/usr/local/Cellar/go/1.4.2/libexec/src/text/template" {
+		t.Errorf("%v|%v|%v", path, dir, err)
+	}
+
+	if err != nil {
+		t.Errorf("%v|%v|%v", path, dir, err)
+	}
+}
+
+func TestQueryParseFailure(t *testing.T) {
+	path, dir, err := queryParse("'text/goog||package text/goog: cannot find package \"text/goog\" in any of:       /usr/local/Cellar/go/1.4.2/libexec/src/text/goog (from $GOROOT)         //go/src/t    ext/goog (from $GOPATH)'")
+
+	if path != "text/goog" {
+		t.Errorf("%v|%v|%v", path, dir, err)
+	}
+
+	if dir != "" {
+		t.Errorf("%v|%v|%v", path, dir, err)
+	}
+
+	if err == nil {
+		t.Errorf("%v|%v|%v", path, dir, err)
+	}
+}
+
+func TestListQueryTemplateSingle(t *testing.T) {
+	expected := sl("'text/template|/usr/local/Cellar/go/1.4.2/libexec/src/text/template|<nil>'")
+	mp, errs := listQueryTemplate("text/template")
+	if !eq(expected, mp) {
+		t.Errorf("%v|%v|%v", expected, mp, errs)
+	}
+}
+
+func TestQuerySingle(t *testing.T) {
+	t.Error("Write me")
+}
+
+func TestQueryMultiple(t *testing.T) {
+	t.Error("Write me")
+}
+
+func TestQueryMultipleWithDupes(t *testing.T) {
+	t.Error("Write me")
+}
+
+func TestQueryFailure(t *testing.T) {
+	t.Error("WRM")
+}
+
+func TestQueryMixedSuccessAndMultipleFailures(t *testing.T) {
+	t.Error("WRM")
+}
