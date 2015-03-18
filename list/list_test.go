73abbd5c4d9d58a844dacaea770e6e70d370c5cd
Kasey Klipsch <kklipsch@mediamath.com>
2015-03-18 11:35:38 -0500

    split the tests that verify 'go list' behavior from cove/list behavior

--- list/list_test.go
+++ list/list_test.go
@@ -1,11 +1,6 @@
 package list
 
-import (
-	"reflect"
-	"testing"
-
-	"github.com/MediaMath/cove/gocmd"
-)
+import "testing"
 
 type JsonResponse struct {
 	ImportPath string
@@ -37,48 +32,6 @@ func TestSingleUnknownResultJson(t *testing.T) {
 	var info JsonResponse
 	if err := Json("os/moo", &info); err == nil {
 		t.Errorf("Should have failed.")
-	}
-}
-
-func TestSinglePathResult(t *testing.T) {
-	cmd, err := gocmd.Go("list", "os/exec").StdOutLines()
-	if !eq(cmd, sl("os/exec")) {
-		t.Errorf("|%v|%v|", cmd, err)
-	}
-}
-
-func TestMultiPathResult(t *testing.T) {
-	cmd, err := gocmd.Go("list", "os/...").StdOutLines()
-	if !eq(cmd, sl("os", "os/exec", "os/signal", "os/user")) {
-		t.Errorf("|%v|%v|", cmd, err)
-	}
-}
-
-func TestFailPathResult(t *testing.T) {
-	cmd, err := gocmd.Go("list", "os/foodog").StdOutLines()
-	if len(cmd) > 0 || err == nil {
-		t.Errorf("|%v|%v|", cmd, err)
-	}
-}
-
-func TestFailPathResultWithPassingToo(t *testing.T) {
-	cmd, err := gocmd.Go("list", "os/foodog", "os/signal").StdOutLines()
-	if !eq(cmd, sl("os/signal")) || err == nil {
-		t.Errorf("|%v|%v|", cmd, err)
-	}
-}
-
-func TestFailPathResultWithErrorsOff(t *testing.T) {
-	cmd, err := gocmd.Go("list", "-e", "os/foodog").StdOutLines()
-	if !eq(cmd, sl("os/foodog")) || err != nil {
-		t.Errorf("|%v|%v|", cmd, err)
-	}
-}
-
-func TestFailPathResultWithPassingTooWithErrorsOff(t *testing.T) {
-	cmd, err := gocmd.Go("list", "-e", "os/foodog", "os/signal").StdOutLines()
-	if !eq(cmd, sl("os/foodog", "os/signal")) || err != nil {
-		t.Errorf("|%v|%v|", cmd, err)
 	}
 }
 
@@ -174,12 +127,4 @@ func count(slice []string, item string) int {
 		}
 	}
 	return c
-}
-
-func eq(slice1 []string, slice2 []string) bool {
-	return reflect.DeepEqual(slice1, slice2)
-}
-
-func sl(items ...string) []string {
-	return items
 }
---
+++ list/spec_test.go
@@ -0,0 +1,59 @@
+package list
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/MediaMath/cove/gocmd"
+)
+
+//These tests verify the behavior of go list
+func TestSinglePathResult(t *testing.T) {
+	cmd, err := gocmd.Go("list", "os/exec").StdOutLines()
+	if !eq(cmd, sl("os/exec")) {
+		t.Errorf("|%v|%v|", cmd, err)
+	}
+}
+
+func TestMultiPathResult(t *testing.T) {
+	cmd, err := gocmd.Go("list", "os/...").StdOutLines()
+	if !eq(cmd, sl("os", "os/exec", "os/signal", "os/user")) {
+		t.Errorf("|%v|%v|", cmd, err)
+	}
+}
+
+func TestFailPathResult(t *testing.T) {
+	cmd, err := gocmd.Go("list", "os/foodog").StdOutLines()
+	if len(cmd) > 0 || err == nil {
+		t.Errorf("|%v|%v|", cmd, err)
+	}
+}
+
+func TestFailPathResultWithPassingToo(t *testing.T) {
+	cmd, err := gocmd.Go("list", "os/foodog", "os/signal").StdOutLines()
+	if !eq(cmd, sl("os/signal")) || err == nil {
+		t.Errorf("|%v|%v|", cmd, err)
+	}
+}
+
+func TestFailPathResultWithErrorsOff(t *testing.T) {
+	cmd, err := gocmd.Go("list", "-e", "os/foodog").StdOutLines()
+	if !eq(cmd, sl("os/foodog")) || err != nil {
+		t.Errorf("|%v|%v|", cmd, err)
+	}
+}
+
+func TestFailPathResultWithPassingTooWithErrorsOff(t *testing.T) {
+	cmd, err := gocmd.Go("list", "-e", "os/foodog", "os/signal").StdOutLines()
+	if !eq(cmd, sl("os/foodog", "os/signal")) || err != nil {
+		t.Errorf("|%v|%v|", cmd, err)
+	}
+}
+
+func eq(slice1 []string, slice2 []string) bool {
+	return reflect.DeepEqual(slice1, slice2)
+}
+
+func sl(items ...string) []string {
+	return items
+}
