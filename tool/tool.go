d40544aa4635c6d5b2af8cf240d570ef1495a973
Kasey Klipsch <kklipsch@mediamath.com>
2015-03-18 22:38:21 -0500

    update report to be better for the script

--- tool/tool.go
+++ tool/tool.go
@@ -3,12 +3,25 @@ package tool
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/MediaMath/cove/gocmd"
 )
 
 // CoverageReport turns the profile into a report using 'go tool cover'
-func CoverageReport(profile string, report string) error {
-	_, err := gocmd.Prepare("tool", "cover", fmt.Sprintf("-html=%s", profile), "-o", report).StdOutLines()
-	return err
+func CoverageReport(profile string, outdir string) (string, error) {
+	report := getFileName(profile, outdir)
+	if _, err := gocmd.Prepare("tool", "cover", fmt.Sprintf("-html=%s", profile), "-o", report).StdOutLines(); err != nil {
+		return "", err
+	}
+
+	return report, nil
+}
+
+func getFileName(profile string, outdir string) string {
+	report := filepath.Base(profile)
+	extension := filepath.Ext(report)
+	name := report[0 : len(report)-len(extension)]
+	fullPath := filepath.Join(outdir, name)
+	return fmt.Sprintf("%s.html", fullPath)
 }
