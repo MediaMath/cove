d007c6648a99d0f161efba2b2c72776b7f6d8015
Kasey Klipsch <kklipsch@mediamath.com>
2015-05-15 11:28:41 -0500

    fix misspelling in documentation

--- go_path.go
+++ go_path.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-//The Go PATH variable can be a ":" sperated list.
+//The Go PATH variable can be a ":" separated list.
 //The default behavior for "most" go tools when needing a canonical
 //go path location is to just use the first one in the list.
 //this function returns that.
